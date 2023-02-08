ded84d662220454d40bfdbff4b38de0ff8f02fe7
Musanzi <musanziw.m@gmail.com>
2023-02-08 13:49:10 +0200

    [INITIAL] Project setup.

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/dashlite.min.css',
+                'resources/js/app.js',
+                'resources/js/bundle.js',
+                'resources/js/scripts.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
