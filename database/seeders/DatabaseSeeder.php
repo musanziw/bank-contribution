4bdcf1b88db54db5cf566b851674dad2ec3aa02d
Musanzi <musanziw.m@gmail.com>
2023-02-17 13:43:24 +0200

    [ADD] Add client.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Agency;
 use App\Models\Bank;
+use App\Models\Client;
 use App\Models\Role;
+use App\Models\Service;
 use App\Models\Subscription;
 use App\Models\Town;
 use App\Models\User;
@@ -24,7 +26,11 @@ class DatabaseSeeder extends Seeder
         Role::upsert([
             ['name' => 'Caissier'], ['name' => 'Collecteur']
         ], ['name']);
-        User::factory(12)->create();
+        Service::upsert([
+            ['name' => 'ChatBot'],
+            ['name' => 'Cotisation'],
+        ], ['name']);
+        User::factory(12)->has(Client::factory(1))->create();
         Town::factory(10)->has(Agency::factory(2))->create();
     }
 }
