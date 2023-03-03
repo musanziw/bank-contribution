a8c6de1d6b58130886a878e29741ab0b2a8379e0
Musanzi <musanziw.m@gmail.com>
2023-03-03 23:49:51 +0200

    [UPDATE] Update logo.

--- resources/views/components/form/auth-card.blade.php
+++ resources/views/components/form/auth-card.blade.php
@@ -5,7 +5,7 @@
         <div class="nk-block nk-block-middle nk-auth-body wide-xs">
             <div class="brand-logo pb-4 text-center">
                 <a href="" class="logo-link">
-                    <img class="logo-dark logo-img logo-img-lg" src="{{ asset('images/bk.jpg') }}"
+                    <img class="logo-dark logo-img logo-img-lg" src="{{ asset('images/bk.jpeg') }}"
                          alt="logo-dark">
                 </a>
             </div>
